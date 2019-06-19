d430e3523183f56ee6dc099f89fc1a5afd92dba8
Mike Morel <mike@techelevator.com>
2019-06-19 12:53:42 -0400

    Module 2 capstone starter

---
+++ 09_Capstone/Capstone/Program.cs
@@ -0,0 +1,28 @@
+﻿using Capstone.Views;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Capstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the connection string from the appsettings.json file
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("Project");
+
+
+
+            // Create a menu and run it
+            SampleMenu menu = new SampleMenu();
+            menu.Run();
+        }
+    }
+}
---
+++ 09_Capstone/Capstone/Views/SampleMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Views
+{
+    /// <summary>
+    /// The top-level menu in our Market Application
+    /// </summary>
+    public class SampleMenu : CLIMenu
+    {
+        /// <summary>
+        /// Constructor adds items to the top-level menu
+        /// </summary>
+        public SampleMenu() : base()
+        {
+            this.Title = "*** Main Menu ***";
+            this.menuOptions.Add("1", "Option One");
+            this.menuOptions.Add("2", "Add two numbers");
+            this.menuOptions.Add("Q", "Quit");
+        }
+
+        /// <summary>
+        /// The override of ExecuteSelection handles whatever selection was made by the user.
+        /// This is where any business logic is executed.
+        /// </summary>
+        /// <param name="choice">"Key" of the user's menu selection</param>
+        /// <returns></returns>
+        protected override bool ExecuteSelection(string choice)
+        {
+            switch (choice)
+            {
+                case "1":
+                    // This is just a sample, does nothing
+                    return true;
+                case "2":
+                    // Get some input form the user, and then do something
+                    int someNumber = GetInteger("Please enter a whole number:");
+                    int anotherNumber = GetInteger("Please enter another whole number:");
+                    Console.WriteLine($"{someNumber} + {anotherNumber} = {someNumber + anotherNumber}.");
+                    Pause("");
+                    return true;
+            }
+            return true;
+        }
+
+        protected override void DisplayBeforeMenu()
+        {
+            Console.WriteLine("*** Use the override DisplayBeforeMenu to display information");
+            Console.WriteLine("*** here, at the top of the screen.  It will be called by the CLI");
+            Console.WriteLine("*** Run method.  Here you might include park information, for example.");
+            Console.WriteLine();
+        }
+
+        protected override void DisplayAfterMenu()
+        {
+            Console.WriteLine("*** You can also display your own text here by overriding DisplayAfterMenu. ***");
+        }
+
+    }
+}
